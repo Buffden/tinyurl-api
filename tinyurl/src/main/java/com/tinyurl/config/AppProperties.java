2f204014faeb501318f68da6d80081068eef696c
Buffden <buffdenslays@gmail.com>
2026-03-29 05:32:21 -0500

    switching CorsConfig.java from @Value to @ConfigurationProperties, which is more refactoring than needed right now. assigning dev profile for local docker compose

--- tinyurl/src/main/java/com/tinyurl/config/AppProperties.java
+++ tinyurl/src/main/java/com/tinyurl/config/AppProperties.java
@@ -2,10 +2,14 @@ package com.tinyurl.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+
 @ConfigurationProperties(prefix = "tinyurl")
 public record AppProperties(
     String baseUrl,
     Integer defaultExpiryDays,
-    Integer shortCodeMinLength
+    Integer shortCodeMinLength,
+    Cors cors
 ) {
+    public record Cors(List<String> allowedOrigins) {}
 }
